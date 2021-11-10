cbf35a3d2210a94fffdb19c20b22dea87a808821
Bados2000 <mimi2222@vp.pl>
2021-11-10 13:34:45 +0100

    error

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 #include <math.h>
 
 using namespace std;
-
-int main()
-{
-
+void kw(){
     double a,b,c,delta,x1,x2,x;
     cout << "Podaj a -> ";
     cin >> a;
@@ -17,31 +14,25 @@ int main()
     cin >> b2;
     if(a2==0 && b2==0){
         cout << "funkcja ma nieskonczenie wiele miejsc zerowych";
-        return 0;
         }
     else if(a2==0 && b2!=0 ){
         cout << " Funkcja nie ma miejsc zerowych";
-        return 0;
     }
     else{
         x0=-b2/a2;
         if(b2>=0){
         cout << "Miejsce zerowe funkcji "<<a2<<"x"<<"+"<<b2<<"  wynosi -> "<<x0;
-        return 0;
         }
         else{
         cout << "Miejsce zerowe funkcji "<<a2<<"x"<<"+("<<b2<<")  wynosi -> "<<x0;
-        return 0;
         }
-    return 0;
-
     }
     }
     cout << "Podaj b  ->  " ;
     cin >> b;
     cout << "Podaj c -> " ;
     cin >> c;
-    delta=(b*b)-(4*a*c); // Wyznaczenie wartoœci tzw.delty -- "b^2-4ac".
+    delta=(b*b)-(4*a*c);
 
 
     if(delta>0)
@@ -59,8 +50,112 @@ int main()
 
      if(delta<0)
     {
-    cout<<"Brak rozwi¹zañ";
+    cout<<"Brak rozwiazan";
+    }
+}
+void liniowa(){
+int a,b,x0;
+    cout << "Podaj a -> ";
+    cin >> a;
+    cout << "Podaj b -> " ;
+    cin >> b;
+    if(a==0 && b==0){
+        cout << "funkcja ma nieskonczenie wiele miejsc zerowych";
+        }
+    else if(a==0 && b!=0 ){
+        cout << " Funkcja nie ma miejsc zerowych";
+    }
+    else{
+        x0=-b/a;
+        if(b>=0)
+        cout << "Miejsce zerowe funkcji "<<a<<"x"<<"+"<<b<<"  wynosi -> "<<x0;
+        else
+        cout << "Miejsce zerowe funkcji "<<a<<"x"<<"+("<<b<<")  wynosi -> "<<x0;
+
+    }
+}
+
+void kwkan(){
+double a,b,c,p,q,delta,x1,x2,x;
+    cout << "Podaj a -> ";
+    cin >> a;
+    if(a==0){
+    double a2,b2,x0;
+    cout << "Podaj b -> ";
+    cin >> a2;
+    cout << "Podaj c -> " ;
+    cin >> b2;
+    if(a2==0 && b2==0){
+        cout << "funkcja ma nieskonczenie wiele miejsc zerowych";
+        }
+    else if(a2==0 && b2!=0 ){
+        cout << " Funkcja nie ma miejsc zerowych";
+    }
+    else{
+        x0=-b2/a2;
+        if(b2>=0){
+        cout << "Miejsce zerowe funkcji "<<a2<<"x"<<"+"<<b2<<"  wynosi -> "<<x0;
+        }
+        else{
+        cout << "Miejsce zerowe funkcji "<<a2<<"x"<<"+("<<b2<<")  wynosi -> "<<x0;
+        }
+    }
+    }
+    else{
+    cout << "Podaj p -> ";
+    cin >> p;
+    cout << "Podaj q -> " ;
+    cin >> q;
+    b=-2*a*p;
+    c=a*p*p+q;
+    delta=(b*b)-(4*a*c);
+
+
+    if(delta>0)
+    {
+        x1=(-b-sqrt(delta))/(2*a);
+        x2=(-b+sqrt(delta))/(2*a);
+        cout<<"Wynik to :\nx1 = "<<x1<<"\nx2 = "<<x2;
     }
 
-    return 0;
+    if(delta==0)
+    {
+        x=-b/(2*a);
+        cout<<"Wynik to : \n x = "<<x;
+    }
+
+     if(delta<0)
+    {
+    cout<<"Brak rozwiazan";
+    }
+    }
+}
+int main()
+{
+
+    int liczba;
+    cout << "----------------Menu wyboru funkcji----------------"<<endl;
+    cout << "1 --- Obliczanie miejsc zerowych funkcji liniowej"<<endl;
+    cout << "2 --- Obliczanie miejsc zerowych funkcji kwadratowej(Postac ogolna)"<<endl;
+    cout << "3 --- Obliczanie miejsc zerowych funkcji kwadratowej(Postac kanoniczna)"<<endl;
+    cout << "Podaj nr operacji ktora chcesz wykonac --> ";
+    do
+    {
+
+        std::cin >> liczba;
+        switch( liczba )
+        {
+        case 1:
+            liniowa();
+            break;
+        case 2:
+            kw();
+            break;
+        case 3:
+            kwkan();
+            break;
+
+        }
+    } while( liczba != 0 );
+
 }
